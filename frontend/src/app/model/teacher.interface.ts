8276620137780c54cb81844a612082016593aaf4
Itsnotjustnate <nathaneduvala@yahoo.com>
2021-05-10 22:39:40 -0700

    more visual updates

--- frontend/src/app/model/teacher.interface.ts
+++ frontend/src/app/model/teacher.interface.ts
@@ -3,6 +3,6 @@ import { Course } from "./course.interface";
 export interface Teacher {
     id: string,
     name: string,
-    courses_taught: Course[],
+    taught: string,
     email: string
 };
