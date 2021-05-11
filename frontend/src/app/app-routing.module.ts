2e389dc881063427c0d6dc403ebcfc8f74b0ad89
Itsnotjustnate <nathaneduvala@yahoo.com>
2021-05-11 12:43:45 -0700

    final updates before the presentation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
-import { UsersComponent } from './components/user/users/users.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 import { UpdateUserProfileComponent } from './components/user/update-user-profile/update-user-profile.component';
 import { CreateBlogEntryComponent } from './components/blog-entry/create-blog-entry/create-blog-entry.component';
